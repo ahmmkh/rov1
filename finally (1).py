b4715e41b7ca74119723a9bc37ffecc45b09318b
ahmmkh <b-b@dr.com>
2016-04-25 06:24:25 -0700

    Added files via upload

---
+++ finally (1).py
@@ -0,0 +1,94 @@
+import pygame
+import time
+import serial
+
+arduino = serial.Serial("COM3",9600,timeout=.1)
+time.sleep(2)
+
+
+pygame.init()
+pygame.joystick.init()
+gamepad = pygame.joystick.Joystick(0)
+gamepad.init()
+
+while pygame.event.poll().type != pygame.QUIT:
+   
+    if gamepad.get_button(6) and gamepad.get_button(3):
+##        print "upwith"
+        arduino.write(b't')
+    elif gamepad.get_button(6) and gamepad.get_button(0):
+##        print 'downwith' 
+    #elif gamepad.get_button(0) == True:
+###        print('down')
+        #arduino.write(b'd')
+       
+        '''
+    elif gamepad.get_button(1) == True:
+        arduino.write(b'x')
+        print('narrowRight')
+    elif gamepad.get_button(2) == True:
+        arduino.write(b'l')
+        print('narrowLeft')
+        '''
+    elif gamepad.get_button(3) == True:
+        arduino.write(b'u')
+##        print('up')
+        '''
+    elif gamepad.get_button(4)==True:
+        print('backward')
+        arduino.write(b'b')
+    elif gamepad.get_button(5)==True:
+        print('forward')
+        arduino.write(b'f')
+        '''
+    #################################        
+    elif gamepad.get_button(6)==True:
+##        print('arm')
+        arduino.write(b'p')
+    elif gamepad.get_button(7)==True:
+##        print('arm')
+        arduino.write(b'i')
+        
+    #################################
+    elif gamepad.get_axis(4) < -0.9:
+##        print "left"
+        arduino.write(b'l ')
+    elif gamepad.get_axis(4) > 0.9:
+##        print "right"
+        arduino.write(b'r')          
+    ##################################
+    elif gamepad.get_axis(1) < -0.9:
+##        print "forward"
+        arduino.write(b'f')
+    elif gamepad.get_axis(1) > 0.9:
+##        print "backward"
+        arduino.write(b'b')
+    elif gamepad.get_axis(2) > 0.05:
+##        print "backward"
+        x = gamepad.get_axis(2) * 255.0
+        z='f'+str(int(x))
+        y = z.encode()
+        arduino.write(y)        
+    #################################
+    elif gamepad.get_hat(0) == (0, -1):
+        arduino.write(b'g')
+##        print('camUp')
+    elif gamepad.get_hat(0) == (0, 1):
+        arduino.write(b'j')
+##        print('camDown')
+    #################################
+    #################################
+    else:
+##        print('Stop Everything!')
+        arduino.write(b'r0')
+    ##################################
+    '''
+    elif gamepad.get_hat(0) == (-1, 0):
+        #print('cameraLeft')
+        arduino.write(b'4')
+    elif gamepad.get_hat(0) == (1, 0):
+        arduino.write(b'6')
+        print('cameraRight')
+        '''
+    ####################################
+    time.sleep(0.1)
